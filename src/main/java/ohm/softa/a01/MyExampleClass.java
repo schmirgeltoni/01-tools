6d6326348ff4716dfc66217994f98e803f986614
schmirgel <MCSIM.SCH@GMAIL.COM>
2025-03-25 00:04:49 +0100

    implement solution

--- src/main/java/ohm/softa/a01/MyExampleClass.java
+++ src/main/java/ohm/softa/a01/MyExampleClass.java
@@ -1,12 +1,12 @@
 package ohm.softa.a01;
 
 public class MyExampleClass {
-	/**
-	 * Basic integer multiplication
-	 * @return product of a and b
-	 */
-	public int multiply(int a, int b) {
-		// TODO return correct multiplication result
-		return 0;
-	}
+    /**
+     * Basic integer multiplication
+     *
+     * @return product of a and b
+     */
+    public int multiply(int a, int b) {
+        return a * b;
+    }
 }
